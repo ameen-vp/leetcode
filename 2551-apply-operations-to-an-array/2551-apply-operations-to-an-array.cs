5a1b167dc16f49e1ff6f6da8a18ad380c1e34310
ameen-vp <muhammedameenvpa@gmail.com>
2025-06-10 08:45:47 +0530

    Time: 4 ms (10%), Space: 47.6 MB (65%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cs
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cs
@@ -1,30 +1,24 @@
+using System;
+using System.Linq;
+
 public class Solution {
     public int[] ApplyOperations(int[] nums) {
-        int n = nums.Length;
-
         
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < nums.Length - 1; i++) {
             if (nums[i] == nums[i + 1]) {
                 nums[i] *= 2;
                 nums[i + 1] = 0;
             }
         }
 
-        
-        List<int> result = new List<int>();
+      
+        var check = nums.Where(x => x != 0).ToList();
 
         
-        foreach (int num in nums) {
-            if (num != 0) {
-                result.Add(num);
-            }
+        while (check.Count < nums.Length) {
+            check.Add(0);
         }
 
-
-        while (result.Count < n) {
-            result.Add(0);
-        }
-
-        return result.ToArray();
+        return check.ToArray();
     }
 }
